761072410e0ed8196116f150f7917195442f0df1
farreledwin <farrel206@gmail.com>
2020-04-18 17:13:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('http://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((data) => this.setState({ monsters: data }));
+	}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						<CardList name="farjun">
+							{this.state.monsters.map((monsters) => <h1 key={monsters.id}>{monsters.name}</h1>)}
+						</CardList>
+					</a>
+					<button onClick={() => this.setState({ string: 'hello farrel' })}>Change text</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
